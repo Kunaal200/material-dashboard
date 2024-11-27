121bb40296d6951e62aab66e2f0246c5158a8d5f
ravs333 <ravs333@gmail.com>
2024-11-27 13:19:13 +0530

    Ajax form check if user exists already

--- front/sign-up.php
+++ front/sign-up.php
@@ -8,6 +8,42 @@ $postData = $_POST;
 
 $requiredFields = array('email', 'password');
 
+if (isset($_POST['action']) && $_POST['action'] == 'signup') {
+    $formData = $_POST;
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $pwd = $_POST['password'];
+
+    //Check if user exists in database with same email id
+    $sql = "SELECT * FROM `users` WHERE `email` = ?";
+    $select = $conn1-> prepare($sql);
+    $select->bind_param("s", $email);
+    $select->execute();
+    $res = $select->get_result();
+    $result = $res->fetch_all(MYSQLI_ASSOC);
+    $select->close();
+
+    // debug_pre($result);
+
+    if(count($result)){
+        $response = array(
+            "success" => false,
+            "message" => "This user already exists.",
+            "data" => []
+        );
+    }else{
+        $response = array(
+            "success" => true,
+            "message" => "",
+            "data" => $result
+        );
+    }
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($response);
+    exit;
+}
+
 if (isset($_POST['submit']) && $_POST['submit'] == 'submitbtn') {
     $error = [];
 
